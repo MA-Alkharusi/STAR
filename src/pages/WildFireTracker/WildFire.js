5e7fdc95a27709310243a109c1402e8d3eca7927
MOHAMMED AHMMED HAMOOD NASSER ALKHARUSI <99081153+MA-Alkharusi@users.noreply.github.com>
2023-12-13 12:27:40 +0000

    home button Mohammed

--- src/pages/WildFireTracker/WildFire.js
+++ src/pages/WildFireTracker/WildFire.js
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import Loader from './components/Loader'
 import Map from './components/Map'
 import './style.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
   const [eventData, setEventData] = useState([])
@@ -21,8 +23,14 @@ function App() {
     fetchEvents()
   }, [])
 
+ const navigate = useNavigate(); 
+const goHome = () => {
+  navigate('/home'); // This will navigate back to the Home page
+};
+
   return (
     <div>
+      <button className="homeButton" onClick={goHome}>STAR</button>
       <Header />
       { !loading ? <Map eventData={eventData} /> : <Loader /> }
     </div>
